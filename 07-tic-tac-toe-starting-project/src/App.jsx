2f061773d5d72a6bfc47bb5b4e346c9906bba853
Daniel <daniel12.souza98@hotmail.com>
2026-02-07 15:20:01 -0300

    codigo video 04-23, vou comentar pelo pc

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import React from "react";
 import GameBoard from "./components/GameBoard.jsx";
 import Player from "./components/Player.jsx";
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  } 
   return (
     // o header foi para o proprio html, para mostra que nada impede de usar html
     // normalmente mesmo com o React, nem tudo precisa ser um componente
+
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X"/>
-          <Player initialName="Player 2" symbol="O"/>
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
     </main>
   );
--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -7,30 +7,32 @@ const initialGameBoard = [
   [null, null, null], //linha 2, coluna 0, 1, 2
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
     // vai fazer uma copia do tabuleiro para poder mudar seus dados
     setGameBoard((prevGameBoard) => {
       // vai atualizar o tabuleiro, colocando todas as linhas e depois as colunas
-      // primeiro ele recebe as linhas no spreed operator e depois com o map ele 
+      // primeiro ele recebe as linhas no spreed operator e depois com o map ele
       // recebe cada conteudo que entraria nas linhas, sendo as colunas
-      const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
       // modifica a coordenada colocando X no lugar de null => 'X'
-      updatedBoard[rowIndex][colIndex] = 'X'
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
-
 
       // pq n pode ser como está abaixo? por causa do principio da imutabilidade
       // você pode ter diversos problemas com dados que são acessados através de referencia
       // quando você deseja muda-los, por isso é necessário fazer uma copia tornando
       // o codigo sem problemas que podem surgir com o tempo
-      // isso ajuda o React no processo de reconciliação (comparar o estado 
+      // isso ajuda o React no processo de reconciliação (comparar o estado
       // antigo com o novo de forma eficiente)
       // prevGameBoard[rowIndex][colIndex] = "X";
       // return prevGameBoard;
     });
+    onSelectSquare();
   }
   return (
     <ol id="game-board">
@@ -41,7 +43,9 @@ export default function GameBoard() {
             {/* segundo .map serve para fazer as colunas passando dentro de cada linha */}
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
