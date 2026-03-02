ba436ac46ecbbed02bf1fa2dc65ec17f938681cd
Daniel <daniel12.souza98@hotmail.com>
2026-03-02 10:58:28 -0300

    esqueci de fazer o git do projeto em branco, aqui foi feito o header no proprio html, o esboço do componente Questions e a criação do arquivo js que tem o quiz

---
+++ 12-demo-project-react-quiz/src/components/Questions.jsx
@@ -0,0 +1,16 @@
+export default function () {
+  return (
+    <div id="quiz">
+      <div id="question">
+        <progress></progress>
+        <h2>Pergunta</h2>
+        <ul id="answers">
+          <li className="answer">
+            {" "}
+            <button>O botao</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
