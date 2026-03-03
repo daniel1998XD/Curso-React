21a0da9333e95d62d1b33cfaa2e2f26247638a8e
Daniel <daniel12.souza98@hotmail.com>
2026-03-03 11:01:37 -0300

    Adicionando randomização da renderização da lista, componente de para o timer com useEffect, consequentement useCallback e usando key para rerenderizar o componente, esta é nova

--- 12-demo-project-react-quiz/src/components/Questions.jsx
+++ 12-demo-project-react-quiz/src/components/Questions.jsx
@@ -1,22 +1,46 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
+
 export default function () {
   const [answersValue, setAnswersValue] = useState([]);
 
   const activeQuestionIndex = answersValue.length;
 
-  function handleSelectAnswer(selectedAnswerw) {
+  const quizComplete = QUESTIONS.length === activeQuestionIndex;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswerw,
+  ) {
     setAnswersValue((prevAnswerValue) => {
       return [...prevAnswerValue, selectedAnswerw];
     });
+  }, []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer],
+  );
+
+  if (quizComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="quiz complete" />
+        <h2>Quiz completed!</h2>
+      </div>
+    );
   }
+
+  const shuffledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswer.sort(() => Math.random() - 0.5);
   return (
     <div id="quiz">
       <div id="question">
-        <progress></progress>
+        <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswer.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
