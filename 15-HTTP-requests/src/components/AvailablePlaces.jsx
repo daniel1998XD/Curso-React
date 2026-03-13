d2a9ea49eb20aaceb1f37878990be7c29b672b70
Daniel <daniel12.souza98@hotmail.com>
2026-03-13 11:02:18 -0300

    prjeto terminado, com busca, adição e remoção de itens com fetch e async functions

--- 15-HTTP-requests/src/components/AvailablePlaces.jsx
+++ 15-HTTP-requests/src/components/AvailablePlaces.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -20,25 +22,30 @@ export default function AvailablePlaces({ onSelectPlace }) {
     async function fetchPlaces() {
       setIsFetching(true);
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
+        const places = await fetchAvailablePlaces();
 
-        if(!response.ok){
-          throw new Error('Falied to fetch places')
-        }
-
-        setAvailablePlaces(resData.places);
-      } catch (error){
-        setError({message:error.message || 'Could not fetch places, please try again later.'})
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude,
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
 
-  if(error) {
-    return <Error title="An error occurred!" message={error.message}/>
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
   }
 
   return (
